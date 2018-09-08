e9bdf3d6be9eda00c0742cd940333832fd2b9dc9
Yadhap Dahal <yadhapdahal111@gmail.com>
2018-09-08 06:21:30 -0400

    added login

--- keys.js
+++ keys.js
@@ -3,3 +3,11 @@ exports.aws = {
     accessKeyId: process.env.ACCESSID,
     region: process.env.REGION
 };
+
+exports.sql = {
+    host:process.env.HOST,
+    port:process.env.PORT,
+    user:process.env.USER,
+    password:process.env.PASSWORD,
+    database:process.env.DATABASE
+};
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,35 @@
 
 var db = require("../models");
+var passport = require("passport");
 
-var apple = "yadhap"; // Test
+
+
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
+  //Creates a new listing
   app.post("/newListing", function(req, res) {
     db.listing.create(req.body).then(() => res.end()).catch(console.error);
   });
 
+  //Create a new user - correct one
+  // app.post("/newUser", function(req, res) {
+  //   db.user.create(req.body).then(() => res.end()).catch(console.error);
+  // });
+
+  //Testing for auth
   app.post("/newUser", function(req, res) {
-    db.user.create(req.body).then(() => res.end()).catch(console.error);
+    db.user.create(req.body).then(function(result){
+      const user_id = result.id;
+      req.login(user_id, function(err){
+        res.redirect("userOptions");
+      });
+      //console.log("$$$$$$$$$$ " + user_id);
+      });
+
   });
 
-
-  //Test:
-  app.get("/searchResults/:location/:food_type", function(req, res){
+   //Test:
+   app.get("/searchResults/:location/:food_type", function(req, res){
     db.listing.findAll({
       where:{
         serving_location: req.params.location,
@@ -46,7 +40,47 @@ module.exports = function(app) {
       res.json(result);
     });
   });
+
+
+
+  //Logging user and auth with passport
+  app.post("/login", passport.authenticate('local', {
+                      successRedirect: '/userOptions',
+                      failureRedirect: '/login'
+                    }
+                  ));
+  
+//Log out user
+app.get("/logout", function(req, res){
+  req.logout();
+  req.session.destroy();
+  res.redirect("/");
+});
+
+  //For displaying food
+  app.get("/searchResults/:location/:food_type", function(req, res){
+    db.listing.findAll({
+      where:{
+        serving_location: req.params.location,
+        food_type: req.params.food_type
+      }
+    }).then( function(result){
+      //console.log(result);
+      //res.json(result);
+    });
+  });
 };
+
+//serelize and deserilize
+passport.serializeUser(function(user_id, done) {
+  done(null, user_id);
+});
+
+passport.deserializeUser(function(user_id, done) {
+    done(null, user_id);
+});
+
+
 
 
 
